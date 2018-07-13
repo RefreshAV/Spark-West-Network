2f052d1b56c5f50c3e709fb0ae5974eaf40985dd
jellycream <maxacaplan@gmail.com>
2018-07-13 09:28:21 -0300

    File Structure

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyDh8N8ZL5QKHNBhEJfovFcJYCGp0vEOews",
+    authDomain: "spark-west.firebaseapp.com",
+    databaseURL: "https://spark-west.firebaseio.com",
+    projectId: "spark-west",
+    storageBucket: "spark-west.appspot.com",
+    messagingSenderId: "13136403971"
+}
