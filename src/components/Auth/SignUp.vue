ba575ed5d4ee0730934843887bf61b168fceec7e
cuppajoeman <callumcn11@gmail.com>
2018-07-17 09:48:39 -0300

    Add phone and twitter authentication

--- src/components/Auth/SignUp.vue
+++ src/components/Auth/SignUp.vue
@@ -17,7 +17,9 @@
         signInOptions: [
           firebase.auth.GoogleAuthProvider.PROVIDER_ID,
           firebase.auth.EmailAuthProvider.PROVIDER_ID,
-          firebase.auth.GithubAuthProvider.PROVIDER_ID
+          firebase.auth.GithubAuthProvider.PROVIDER_ID,
+          firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+          firebase.auth.PhoneAuthProvider.PROVIDER_ID
         ]
       };
       var ui = new firebaseui.auth.AuthUI(firebase.auth());
