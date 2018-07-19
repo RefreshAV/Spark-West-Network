5d9e81ee62b67114e10b7be981924217a1ad2571
jellycream <maxacaplan@gmail.com>
2018-07-19 08:24:28 -0300

    Events page scalability added

--- src/components/Events/Events.vue
+++ src/components/Events/Events.vue
@@ -3,7 +3,7 @@
     <div class="d-flex justify-content-center" v-if="events.length == 0">
       <img src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Ajax_loader_metal_512.gif" class="loading" style="width:50px; height: 50px;">
     </div>
-    <!-- <div v-if="events.length != 0">
+    <div v-if="events.length != 0">
       <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
       <div class="col-md-6 px-0">
         <h1 class="display-4">{{events[0].title}}</h1>
@@ -12,72 +12,26 @@
       </div>
     </div>
 
-    <div class="row mb-2">
-      <div class="col-md-6">
-        <div class="card flex-md-row mb-4 box-shadow h-md-250">
+    <div class="card-deck mb-2">
+        <div class="card flex-md-row mb-4 box-shadow h-md-250" style="min-width:30rem;" v-for="(event, i) in events.slice(1)" v-bind:key="event.id">
           <div class="card-body d-flex flex-column align-items-start">
-            <h3 class="mb-0">
-              {{events[1].title}}
-            </h3>
-            <div class="mb-1 text-muted">{{events[1].date}}</div>
-            <p class="card-text mb-auto">{{events[1].desc}}</p>
-            <router-link v-bind:to="{name: 'event-detail', params: {id: events[1].id}}" tag="a" active-class="nav-item">Find out more...</router-link>
-
-          </div>
-          <img class="card-img-right flex-auto d-none d-md-block"  v-if="images.length > 0" :src="images[1]" alt="Card image cap">
-          <img class="card-img-right flex-auto d-none d-md-block" v-if="images.length == 0" src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Ajax_loader_metal_512.gif" alt="Card image cap">
-        </div>
-      </div>
-      <div class="col-md-6">
-        <div class="card flex-md-row mb-4 box-shadow h-md-250">
-          <div class="card-body d-flex flex-column align-items-start">
-            <h3 class="mb-0">
-              {{events[2].title}}
-            </h3>
-            <div class="mb-1 text-muted">{{events[2].date}}</div>
-            <p class="card-text mb-auto">{{events[2].desc}}</p>
-            <router-link v-bind:to="{name: 'event-detail', params: {id: events[2].id}}" tag="a" active-class="nav-item">Find out more...</router-link>
-          </div>
-          <img class="card-img-right flex-auto d-none d-md-block" v-if="images.length > 0" :src="images[2]" alt="Card image cap">
-          <img class="card-img-right flex-auto d-none d-md-block" v-if="images.length == 0" src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Ajax_loader_metal_512.gif" alt="Card image cap">
-        </div>
-      </div>
-    </div>
-    </div> -->
-
-    <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
-      <div class="col-md-6 px-0">
-        <h1 class="display-4">{{events[0].title}}</h1>
-        <p class="lead my-3">{{events[0].desc}}</p>
-        <p class="lead mb-0"><router-link v-bind:to="{name: 'event-detail', params: {id: events[0].id}}" tag="a" class="text-white font-weight-bold">Find out more...</router-link></p>
-      </div>
-    </div>
-
-    
-
-<div class="card-deck">
-        <div class="card flex-md-row mb-4 box-shadow h-md-250" v-for="(event,i) in events.slice(1)" v-bind:key="event.id" style="min-width:30rem;">
-          <div class="card-body" style="min-width:50%;">
             <h3 class="mb-0">
               {{event.title}}
             </h3>
             <div class="mb-1 text-muted">{{event.date}}</div>
             <p class="card-text mb-auto">{{event.desc}}</p>
-            <router-link v-bind:to="{name: 'event-detail', params: {id: events.id}}" tag="a" active-class="nav-item">Find out more...</router-link>
+            <router-link v-bind:to="{name: 'event-detail', params: {id: event.id}}" tag="a" active-class="nav-item">Find out more...</router-link>
 
           </div>
-          <div id="eventImg">
-            <img class="card-img-right flex-auto d-none d-md-block" v-if="images.length > 0" :src="images[i]" alt="Card image cap">
+          <img class="card-img-right flex-auto d-none d-md-block"  v-if="images.length > 0" :src="images[i+1]" alt="Card image cap">
           <img class="card-img-right flex-auto d-none d-md-block" v-if="images.length == 0" src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Ajax_loader_metal_512.gif" alt="Card image cap">
-          </div>
-        </div>     
-</div>
-
-
-    <div class="text-center mb-3">
-      <router-link to="/events/NewEvent" tag="button" class="btn btn-primary" v-if="isLoggedIn">Create an Event!</router-link>
-      <router-link to="/events/list" tag="button" class="btn btn-secondary" v-if="isLoggedIn">All Events!</router-link>
-    </div> 
+        </div>
+    </div>
+    <div class="text-center">
+      <router-link to="/events/NewEvent" tag="button" class="btn btn-primary my-2" v-if="isLoggedIn">Create an Event!</router-link>
+      <router-link to="/events/list" tag="button" class="btn btn-secondary my-2" v-if="isLoggedIn">All Events!</router-link>
+    </div>
+    </div>
   </div>
 </template>
 
@@ -96,7 +50,7 @@ export default {
     db
       .collection("events")
       .orderBy("event.date")
-      .limit(7)
+      .limit(3)
       .get()
       .then(querySnapshot => {
         querySnapshot.forEach(doc => {
@@ -127,7 +81,6 @@ export default {
   methods: {
     fetchImage() {
       var images = [];
-
       for (var i = 0; i < this.events.length; i++) {
         var url =
           "https://firebasestorage.googleapis.com/v0/b/spark-west.appspot.com/o/events%2F" +
@@ -143,27 +96,23 @@ export default {
 
 <style scoped>
 /* stylelint-disable selector-list-comma-newline-after */
-
-#eventImg {
-  overflow: hidden;
-}
-#eventImg img{
-  min-height: 100%;
-}
-
 .blog-header {
   line-height: 1;
   border-bottom: 1px solid #e5e5e5;
 }
-
 .blog-header-logo {
   font-size: 2.25rem;
 }
-
 .blog-header-logo:hover {
   text-decoration: none;
 }
-
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+}
 .display-4 {
   font-size: 2.5rem;
 }
@@ -172,14 +121,12 @@ export default {
     font-size: 3rem;
   }
 }
-
 .nav-scroller {
   position: relative;
   z-index: 2;
   height: 2.75rem;
   overflow-y: hidden;
 }
-
 .nav-scroller .nav {
   display: -webkit-box;
   display: -ms-flexbox;
@@ -193,25 +140,21 @@ export default {
   white-space: nowrap;
   -webkit-overflow-scrolling: touch;
 }
-
 .nav-scroller .nav-link {
   padding-top: 0.75rem;
   padding-bottom: 0.75rem;
   font-size: 0.875rem;
 }
-
 .card-img-right {
   height: 100%;
   width: 50%;
   border-radius: 0 3px 3px 0;
 }
-
 .flex-auto {
   -ms-flex: 0 0 auto;
   -webkit-box-flex: 0;
   flex: 0 0 auto;
 }
-
 .h-250 {
   height: 250px;
 }
@@ -220,18 +163,15 @@ export default {
     height: 250px;
   }
 }
-
 .border-top {
   border-top: 1px solid #e5e5e5;
 }
 .border-bottom {
   border-bottom: 1px solid #e5e5e5;
 }
-
 .box-shadow {
   box-shadow: 0 0.25rem 0.75rem rgba(0, 0, 0, 0.05);
 }
-
 /*
    * Blog name and description
    */
@@ -244,13 +184,11 @@ export default {
   font-size: 1.1rem;
   color: #999;
 }
-
 @media (min-width: 40em) {
   .blog-title {
     font-size: 3.5rem;
   }
 }
-
 /* Pagination */
 .blog-pagination {
   margin-bottom: 4rem;
@@ -258,7 +196,6 @@ export default {
 .blog-pagination > .btn {
   border-radius: 2rem;
 }
-
 /*
    * Blog posts
    */
@@ -273,7 +210,6 @@ export default {
   margin-bottom: 1.25rem;
   color: #999;
 }
-
 /*
    * Footer
    */
