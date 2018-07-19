c0e0fbe946199ec04587158bb5d49feb93cacea7
cuppajoeman <callumcn11@gmail.com>
2018-07-19 11:31:31 -0300

    add user to user collection on successful signup, load name and avatar on profile page

--- src/components/Auth/SignUpSuccess.vue
+++ src/components/Auth/SignUpSuccess.vue
@@ -3,42 +3,59 @@
     <h1>Signup succeeded</h1>
     <button @click='logOut' class="btn btn-primary">Log out</button>
     <hr>
-    <img :src="photo" style=”height:120px”> <br>
-    <p>{{name}}</p>
-    <p>{{email}}</p>
-    <p>{{userId}}</p>
+    <img :src="user.photo" style=”height:120px”> <br>
+    <p>{{user.name}}</p>
+    <p>{{user.email}}</p>
+    <p>{{user.UserUID}}</p>
+    <button @click="uploadData" class="btn btn-primary">Continue</button>
     <hr>
-    <pre>{{user}}</pre>
+    <!--<pre>{{user}}</pre>-->
   </div>
 </template>
 
 <script>
+  import db from "../../Firebase/firebaseInit";
   import firebase from 'firebase/app'
   export default {
     data(){
       return {
-        photo: '',
-        userId: '',
-        name: '',
-        email: '',
-        user: {},
+        user: {
+          photo: '',
+          UserUID: '',
+          name: '',
+          email: '',
+        }
       }
     },
     created() {
       var vm = this
       firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
-          vm.user = user;
-          vm.name = vm.user.displayName;
-          vm.email = vm.user.email;
-          vm.photo = vm.user.photoURL;
-          vm.userId = vm.user.uid;
+          vm.user.name = user.displayName;
+          vm.user.email = user.email;
+          vm.user.photo = user.photoURL;
+          vm.user.UserUID = user.uid;
         }
       });
+    },
+    updated() {
     },
     methods: {
       logOut() {
         firebase.auth().signOut();
+      },
+      uploadData() {
+        db
+          .collection("users")
+          .add({
+            user: {
+              name: this.user.name,
+              email: this.user.email,
+              photo: this.user.photo,
+              UserUID: this.user.UserUID
+            }
+          });
+        this.$router.push("/");
       }
     }
   };
