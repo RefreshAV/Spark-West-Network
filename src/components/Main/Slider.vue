10efd9a517fbd778e343cd936dc586cfc69ea075
jellycream <maxacaplan@gmail.com>
2018-07-23 11:15:47 -0300

    Changed placeholder images

--- src/components/Main/Slider.vue
+++ src/components/Main/Slider.vue
@@ -11,7 +11,7 @@
       </ol>
       <div class="carousel-inner" data-interval="false" role="listbox">
         <!-- Slide One - Set the background image for this slide in the line below -->
-        <div class="carousel-item active" style="background-image: url('http://placehold.it/1900x1080')">
+        <div class="carousel-item active" style="background-image: url('https://picsum.photos/1900/1080/?random')">
           <div class="carousel-caption d-none d-md-block">
             <h3>First Image Slide</h3>
             <p>This is a description for the first image.</p>
@@ -34,7 +34,7 @@
           </div>
         </div>
 
-        <div class="carousel-item" style="background-image: url('http://placehold.it/1900x1080')">
+        <div class="carousel-item" style="background-image: url('https://picsum.photos/1900/1080/?random')">
           <div class="carousel-caption d-none d-md-block">
             <h3>Second Image Slide</h3>
             <p>This is a description for the second image.</p>
@@ -57,7 +57,7 @@
           </div>
         </div>
 
-        <div class="carousel-item" style="background-image: url('http://placehold.it/1900x1080')">
+        <div class="carousel-item" style="background-image: url('https://picsum.photos/1900/1080/?random')">
           <div class="carousel-caption d-none d-md-block">
             <h3>Third Image Slide</h3>
             <p>This is a description for the third image.</p>
