cc6726f8f78dda8841b51f1198305b718ec14fd9
jellycream <maxacaplan@gmail.com>
2018-08-25 15:41:49 -0300

    Scroll bug fixed

--- src/components/Auth/SignUpSuccess.vue
+++ src/components/Auth/SignUpSuccess.vue
@@ -66,7 +66,6 @@ export default {
                 });
               }
             } else {
-              console.log("Doc Exists");
             }
           });
       });
--- src/main.js
+++ src/main.js
@@ -20,7 +20,10 @@ Vue.use(VueRouter);
 
 const router = new VueRouter({
   routes,
-  mode: 'history'
+  mode: 'history',
+  scrollBehavior (to, from, savedPosition) {
+    return { x: 0, y: 0 }
+  }
 });
 
 // Router guard checks if AuthRequired
