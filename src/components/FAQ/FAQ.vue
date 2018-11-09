1a526302498becf622ef047bbb7de699b95613a9
jellycream <maxacaplan@gmail.com>
2018-11-09 19:12:05 -0400

    FAQ filled out

--- src/components/FAQ/FAQ.vue
+++ src/components/FAQ/FAQ.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="container mb-4">
     <h1 class="mt-4 mb-3">FAQ
     </h1>
     <div id="accordion">
@@ -14,7 +14,7 @@
               data-target="#collapseOne"
               aria-expanded="true"
               aria-controls="collapseOne">
-              Question #1
+              Why should this initiative be focused on a small rural area like the Annapolis Valley?
             </button>
           </h5>
         </div>
@@ -25,7 +25,7 @@
           aria-labelledby="headingOne"
           data-parent="#accordion">
           <div class="card-body">
-            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            This initiative wants to make western rural Nova Scotia innovate, create businesses, and urbanize. We believe there is greater value in engaging the geographic region targeted by the Innovacorp Spark West Innovation Challenge. This includes Lunenburg, Queens, Shelburne, Yarmouth, Digby, Annapolis, Kings, and Hants counties. We all share alike this rural region of Nova Scotia and collaborate across this region regularly, and our collective footprint provides a more meaningful picture. 
           </div>
         </div>
       </div>
@@ -40,7 +40,7 @@
               data-target="#collapseTwo"
               aria-expanded="false"
               aria-controls="collapseTwo">
-              Question #2
+              Should we not just promote our region’s activities under the Halifax Startup Digest?
             </button>
           </h5>
         </div>
@@ -50,7 +50,7 @@
           aria-labelledby="headingTwo"
           data-parent="#accordion">
           <div class="card-body">
-            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            We should absolutely promote our region’s activities to the wider Nova Scotia startup community. HSD is an excellent way to do this. However, HSD has its own mandate which does not include our rural Nova Scotia ecosystem. It is a rare experience to find content in HSD that pulls from our region. Furthermore, HSD does not have a good vantage point to attempt to adequately represent Western Nova, because it is the big urban centres like Halifax that engage and are rich in innovation, business, entrepreneurship, and such.
           </div>
         </div>
       </div>
@@ -65,7 +65,7 @@
               data-target="#collapseThree"
               aria-expanded="false"
               aria-controls="collapseThree">
-              Question #3
+              Where do you draw your inspiration from?
             </button>
           </h5>
         </div>
@@ -75,7 +75,10 @@
           aria-labelledby="headingThree"
           data-parent="#accordion">
           <div class="card-body">
-            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            Halifax has done a remarkable job drawing people in and sustaining a culture that appeals to entrepreneurs and creative professionals. One initiative we want to draw attention to is the Halifax Startup Digest (HSD), run by John Hamblin. 
+            <br>
+            <br>
+            HSD is a twice-monthly email newsletter and web calendar that aggregates events of interest to this Halifax community. As a cross-organization voice, HSD pulls together a compelling picture of the wide range of activities and organizations that make Halifax the dynamic startup ecosystem that it is. Furthermore, HSD draws from relevant programs being delivered in other urban centres in the Atlantic region, and beyond, to help situate Halifax within a larger context. As a volunteer program run by one individual, it is quite an effective community building initiative.
           </div>
         </div>
       </div>
