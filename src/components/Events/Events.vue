16afe4564d9c1fd7ee2a153dbdab8b94c46953ec
jellycream <maxacaplan@gmail.com>
2018-11-09 23:04:35 -0400

    Events page rework

--- src/components/Events/Events.vue
+++ src/components/Events/Events.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container mt-3">
+  <div class="container my-3">
     <div
       class="d-flex justify-content-center"
       v-if="events.length == 0">
@@ -9,8 +9,8 @@
         style="width:50px; height: 50px;">
     </div>
     <div v-if="events.length != 0">
-      <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
-        <div class="col-md-6 px-0">
+      <!-- <div class="jumbotron text-white rounded" :style="'background: url(' + images[0] + ')'">
+        <div id="jumboContent" class="col-md-4 bg-dark">
           <h1 class="display-4">{{ events[0].title }}</h1>
           <p class="lead my-3">{{ events[0].desc }}</p>
           <p class="lead mb-0"><router-link
@@ -18,9 +18,43 @@
             tag="a"
             class="text-white font-weight-bold">Find out more...</router-link></p>
         </div>
+      </div> -->
+
+      <div class="card mb-3 bg-dark text-white">
+        <div class="row no-gutters">
+          <div class="col">
+            <div class="card-block m-3 px-2">
+              <h1 class="display-4">{{ events[0].title }}</h1>
+              <p class="my-3">{{ events[0].desc }}</p>
+              <p class="mb-0"><router-link
+                :to="{name: 'event-detail', params: {id: events[0].id}}"
+                tag="a"
+                class="text-white font-weight-bold">Find out more...</router-link></p>
+            </div>
+          </div>
+          <div class="col-4" :style="'background: url(' + images[0] + '); background-size:cover; background-position:center; background-repeat: no-repeat;'">
+          </div>
+        </div>
       </div>
 
-      <div class="card-deck mb-2">
+      <div class="card-deck mb-3">
+        <router-link :to="{name: 'event-detail', params: {id: events[1].id}}" class="card bg-light text-dark">
+          <div class="card-body">
+            <h1>{{ events[1].title }}</h1>
+            <hr>
+            <p class="my-3">{{ events[1].desc }}</p>
+          </div>
+        </router-link>
+        <router-link :to="{name: 'event-detail', params: {id: events[2].id}}" class="card bg-light text-dark">
+          <div class="card-body">
+            <h1>{{ events[2].title }}</h1>
+            <hr>
+            <p class="my-3">{{ events[2].desc }}</p>
+          </div>
+        </router-link>
+      </div>
+
+      <!-- <div class="card-deck mb-2">
         <router-link
           :to="{name: 'event-detail', params: {id: event.id}}"
           class="card flex-md-row mb-4 box-shadow h-md-250"
@@ -47,219 +81,131 @@
             src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Ajax_loader_metal_512.gif"
             alt="Card image cap">
         </router-link>
-      </div>
+      </div> -->
+
       <div class="text-center">
         <router-link
           to="/events/NewEvent"
           tag="button"
-          class="btn btn-primary my-2"
-          v-if="isLoggedIn">Create an Event!</router-link>
+          class="btn btn-primary">
+          Create an Event!</router-link>
         <router-link
           to="/events/list"
           tag="button"
-          class="btn btn-secondary my-2"
-          v-if="isLoggedIn">All Events!</router-link>
+          class="btn btn-secondary mx-1">
+          All Events!</router-link>
         <router-link
           to="/events/EventMap"
           tag="button"
-          class="btn btn-secondary my-2"
-          v-if="isLoggedIn">Map</router-link>
+          class="btn btn-secondary">
+          Map</router-link>
       </div>
     </div>
   </div>
 </template>
 
 <script scoped>
-import db from '../../Firebase/firebaseInit'
-import firebase from 'firebase'
+import db from "../../Firebase/firebaseInit";
+import firebase from "firebase";
 
 export default {
-  data () {
+  data() {
     return {
       events: [],
       images: [],
       isLoggedIn: false
-    }
+    };
   },
-  created () {
-    db
-      .collection('events')
-      .orderBy('likes', 'desc')
+  created() {
+    db.collection("events")
+      .orderBy("likes", "desc")
       .limit(3)
       .get()
       .then(querySnapshot => {
         querySnapshot.forEach(doc => {
           // truncate description
-          var desc
-          if (doc.data().event.description.length > 130 && this.events.length > 0) {
-            var trunc = doc.data().event.description
-            desc = trunc.substring(0, 131) + ' . . .'
+          var desc;
+          if (
+            doc.data().event.description.length > 180 &&
+            this.events.length > 0
+          ) {
+            var trunc = doc.data().event.description;
+            desc = trunc.substring(0, 181) + " . . .";
           } else {
-            desc = doc.data().event.description
+            desc = doc.data().event.description;
           }
 
           // compile and push data as object
+          var day = doc.data().event.date.day
+          var month = doc.data().event.date.month
+          var year = doc.data().event.date.year
+
           const data = {
             id: doc.id,
             title: doc.data().event.title,
-            date: doc.data().event.date,
+            date: year + '-' + month + '-' + day,
             time: doc.data().event.time,
             email: doc.data().event.email,
             desc: desc,
             imageKey: doc.data().event.imageKey
-          }
-          this.events.push(data)
-        })
-      })
-    var vm = this
-    firebase.auth().onAuthStateChanged(function (user) {
+          };
+          this.events.push(data);
+        });
+      });
+    id = "bigTxt";
+    var vm = this;
+    firebase.auth().onAuthStateChanged(function(user) {
       if (user) {
-        vm.isLoggedIn = true
+        vm.isLoggedIn = true;
       } else {
-        vm.isLoggedIn = false
+        vm.isLoggedIn = false;
       }
-    })
+    });
   },
   watch: {
-    events: 'fetchImage'
+    events: "fetchImage"
   },
   methods: {
-    fetchImage () {
-      var images = []
+    fetchImage() {
+      var images = [];
       for (var i = 0; i < this.events.length; i++) {
         var url =
-            'https://firebasestorage.googleapis.com/v0/b/spark-west.appspot.com/o/events%2F' +
-            this.events[i].imageKey +
-            '?alt=media&token'
-        images.push(url)
+          "https://firebasestorage.googleapis.com/v0/b/spark-west.appspot.com/o/events%2F" +
+          this.events[i].imageKey +
+          "?alt=media&token";
+        images.push(url);
       }
-      this.images = images
+      this.images = images;
     }
   }
-}
+};
 </script>
 
 <style scoped>
-  /* stylelint-disable selector-list-comma-newline-after */
+/* .jumbotron {
+  padding: 0;
+  height: 20rem;
+}
 
-  .blog-header {
-    line-height: 1;
-    border-bottom: 1px solid #e5e5e5;
-  }
-  .blog-header-logo {
-    font-size: 2.25rem;
-  }
-  .blog-header-logo:hover {
-    text-decoration: none;
-  }
-  .display-4 {
-    font-size: 2.5rem;
-  }
-  @media (min-width: 768px) {
-    .display-4 {
-      font-size: 3rem;
-    }
-  }
-  .nav-scroller {
-    position: relative;
-    z-index: 2;
-    height: 2.75rem;
-    overflow-y: hidden;
-  }
-  .nav-scroller .nav {
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-    -ms-flex-wrap: nowrap;
-    flex-wrap: nowrap;
-    padding-bottom: 1rem;
-    margin-top: -1px;
-    overflow-x: auto;
-    text-align: center;
-    white-space: nowrap;
-    -webkit-overflow-scrolling: touch;
-  }
-  .nav-scroller .nav-link {
-    padding-top: 0.75rem;
-    padding-bottom: 0.75rem;
-    font-size: 0.875rem;
-  }
-  .card-img-right {
-    height: 100%;
-    width: 50%;
-    border-radius: 0 3px 3px 0;
-  }
-  .flex-auto {
-    -ms-flex: 0 0 auto;
-    -webkit-box-flex: 0;
-    flex: 0 0 auto;
-  }
-  .h-250 {
-    height: 250px;
-  }
-  @media (min-width: 768px) {
-    .h-md-250 {
-      height: 250px;
-    }
-  }
-  .border-top {
-    border-top: 1px solid #e5e5e5;
-  }
-  .border-bottom {
-    border-bottom: 1px solid #e5e5e5;
-  }
-  .box-shadow {
-    box-shadow: 0 0.25rem 0.75rem rgba(0, 0, 0, 0.05);
-  }
-  /*
-     * Blog name and description
-     */
-  .blog-title {
-    margin-bottom: 0;
-    font-size: 2rem;
-    font-weight: 400;
-  }
-  .blog-description {
-    font-size: 1.1rem;
-    color: #999;
-  }
-  @media (min-width: 40em) {
-    .blog-title {
-      font-size: 3.5rem;
-    }
-  }
-  /* Pagination */
-  .blog-pagination {
-    margin-bottom: 4rem;
-  }
-  .blog-pagination > .btn {
-    border-radius: 2rem;
-  }
-  /*
-     * Blog posts
-     */
-  .blog-post {
-    margin-bottom: 4rem;
-  }
-  .blog-post-title {
-    margin-bottom: 0.25rem;
-    font-size: 2.5rem;
-  }
-  .blog-post-meta {
-    margin-bottom: 1.25rem;
-    color: #999;
-  }
-  /*
-     * Footer
-     */
-  .blog-footer {
-    padding: 2.5rem 0;
-    color: #999;
-    text-align: center;
-    background-color: #f9f9f9;
-    border-top: 0.05rem solid #e5e5e5;
-  }
-  .blog-footer p:last-child {
-    margin-bottom: 0;
-  }
+#jumboContent {
+  height: 100%;
+}
+
+#bigEvent {
+  padding: 0;
+  max-height: 20rem;
+  height: 100%;
+}
+
+#bigTxt {
+  overflow: hidden;
+  text-overflow: ellipsis; 
+}
+
+#bigImg {
+  padding: 0;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+} */
 </style>
