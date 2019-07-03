02fe47decaac1212fd08c8fd3911e05b2460908c
jellycream <maxacaplan@gmail.com>
2019-07-03 12:01:13 -0300

    User list event numbering added

--- src/components/Profile/UserList.vue
+++ src/components/Profile/UserList.vue
@@ -24,54 +24,84 @@
         <router-link
           v-if="profile.isProfile === false"
           :to="{name: 'userDetail', params: {id: profile.id}}"
-          class="media btn btn-light shadow-sm"
+          class="media border btn btn-light shadow-sm p-2"
         >
-          <div v-if="!profile.img" class="spinnerWrapper d-flex justify-content-center align-items-center rounded border bg-white">
+          <div
+            v-if="!profile.img"
+            class="spinnerWrapper d-flex justify-content-center align-items-center rounded border bg-white"
+          >
             <div class="spinner-border spinner-border-sm w-50 h-50" role="status">
               <span class="sr-only">Loading...</span>
             </div>
           </div>
           <img
-            class="align-self-center rounded mr-3"
+            class="align-self-center mr-3"
             v-if="profile.img"
             :src="profile.img"
             alt="profile image"
             width="84"
             height="84"
-          >
+          />
           <div class="media-body">
             <h5 class="mb-0">{{ profile.name }}</h5>
             <p class="mb-0">{{ profile.about }}</p>
-            <span class="badge badge-primary">
-              <i class="fa fa-user"/> n Followers
-            </span>
+            <div class="row">
+              <div class="col">
+                <span class="badge badge-primary">
+                  <i class="fa fa-user" /> n Followers
+                </span>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col">
+                <span class="badge badge-pill badge-info">
+                  <i class="fa fa-calendar" />
+                  {{ profile.events }}
+                </span>
+              </div>
+            </div>
           </div>
         </router-link>
 
         <router-link
           v-if="profile.isProfile === true"
           :to="{name: 'profile'}"
-          class="media btn btn-light shadow-sm"
+          class="media border btn btn-light shadow-sm"
         >
-          <div v-if="!profile.img" class="spinnerWrapper d-flex justify-content-center align-items-center rounded border bg-white">
+          <div
+            v-if="!profile.img"
+            class="spinnerWrapper d-flex justify-content-center align-items-center rounded border bg-white"
+          >
             <div class="spinner-border spinner-border-sm w-50 h-50" role="status">
               <span class="sr-only">Loading...</span>
             </div>
           </div>
           <img
-            class="align-self-center rounded mr-3"
+            class="align-self-center mr-3"
             v-if="profile.img"
             :src="profile.img"
             alt="profile image"
             width="84"
             height="84"
-          >
+          />
           <div class="media-body">
             <h5 class="mb-0">{{ profile.name }}</h5>
             <p class="mb-0">{{ profile.about }}</p>
-            <span class="badge badge-primary">
-              <i class="fa fa-user"/> n Followers
-            </span>
+            <div class="row">
+              <div class="col">
+                <span class="badge badge-primary">
+                  <i class="fa fa-user" /> n Followers
+                </span>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col">
+                <span class="badge badge-pill badge-info">
+                  <i class="fa fa-calendar" />
+                  {{ profile.events }}
+                </span>
+              </div>
+            </div>
           </div>
         </router-link>
       </div>
@@ -87,6 +117,7 @@ export default {
   data() {
     return {
       profiles: [],
+      events: [],
       render: false
     };
   },
@@ -117,6 +148,7 @@ export default {
             img: doc.data().user.photo,
             email: doc.data().user.email,
             about: doc.data().user.about,
+            events: 0,
             isProfile: isProfile
           };
 
@@ -125,11 +157,42 @@ export default {
           }
           this.profiles.push(data);
         });
-      });
+      })
+      .then(
+        db
+          .collection("events")
+          .get()
+          .then(querySnapshot => {
+            querySnapshot.forEach(doc => {
+              this.events.push({
+                uid: doc.data().event.UserUID
+              });
+            });
+          })
+          .then(() => {
+            console.log(this.profiles.length);
+            for (let i = 0; i < this.profiles.length; i++) {
+              let uid = this.profiles[i].uid;
+              let num = this.events.filter(event => event.uid === uid);
+              this.profiles[i].events = num.length;
+            }
+          })
+      );
   },
+  // mounted() {
+  //   let i = 0
+  //   for (i = 0; i < this.profiles.length; i++) {
+  //     // let num = this.events.filter(event => event.uid === profile.uid).length;
+  //     console.log(i);
+  //   }
+  //   console.log(i)
+  // },
   methods: {
     loadProfile(uid, id) {
       this.$router.push("Users/user/" + id);
+    },
+    countEvents(profile) {
+      console.log("loaded");
     }
   }
 };
@@ -138,6 +201,11 @@ export default {
 <style scoped>
 img {
   width: 84px;
+  border-radius: 12px;
+}
+
+.media {
+  border-radius: 12px;
 }
 
 .btn-light {
