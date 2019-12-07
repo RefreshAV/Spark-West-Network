fb53cc9e624d1a55ed98f5adcabd9a4e7e0f9745
jellycream <maxacaplan@gmail.com>
2019-12-06 20:51:18 -0400

    bio update

--- src/components/About/About.vue
+++ src/components/About/About.vue
@@ -183,7 +183,7 @@
             <div class="row">
               <div class="col">
                 <hr />
-                <p>Alexander Petkov is a high school student who attends Horton High School and is one of the co-founders of Spark West Network, a non-profit organization that aims to pull together the diverse organizations, events, and opportunities present in the Annapolis Valley. He is also the Chief Marketing Officer for Refresh Annapolis Valley. Upon meeting Alex, many people describe him as robotic because they say he looks and acts very serious. This is far from the truth. In fact, Alex behaves very humanly and has many human-like interests and engages in many human-like activities. He is an intermediate-level Python programmer and enjoys tinkering with electronics. He is also passionate about entrepreneurship, computer engineering, and world history. In his free time, he engages in typical human-like recreational activities such as watching YouTube or Netflix or playing video games. Also, Alex can and will kick your ass in any hack and slash, stealth, or grand strategy game.</p>
+                <p>Alexander Petkov is a hard-working student at Horton High School and is one of the co-founders of the Spark West Network, a non-profit organization that aims to pull together the diverse organizations, events, and opportunities present in the Annapolis Valley. He is also the Chief Marketing Officer for Refresh Annapolis Valley. Alex is an experienced Python programmer and enjoys tinkering with electronics. He enjoys boxing and is passionate about science and world history, as well as entrepreneurship. He is currently in the International Baccalaureate Diploma Program at Horton High School.</p>
               </div>
             </div>
           </div>
